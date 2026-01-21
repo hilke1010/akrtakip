3a3c9e42f98ff41edd9a8216ed37bd26541a9559
hilke1010 <haritatakip10@gmail.com>
2026-01-21 10:23:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def main():
     df, target_date_col, start_date_col = data_result
 
     with st.sidebar:
-        st.info("🕒 Veriler her gün saat 10:00'da yenilenmektedir. (Dosya yükleme süresi 5 dakika)")
+        st.info("🕒 Veriler her gün saat 10:00'da yenilenmektedir. (Talebiniz olması halinde anında yenilenebilir veriler)")
         st.markdown("---")
         st.title("🔍 Filtre Paneli")
         
@@ -737,3 +737,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
